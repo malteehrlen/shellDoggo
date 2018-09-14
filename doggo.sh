a78dc1cb0ecfbb63e24c1309322fa2b5b24de080
Malte Ehrlen <malte.ehrlen@biomagnetik.com>
2018-09-14 09:35:28 +0200

    added more functionality and fixed a bug when another user was present while doggo launched. Doggo will now clean up after himself but not commit suicide by default

--- doggo.sh
+++ doggo.sh
@@ -11,8 +11,16 @@ do
 sleep 5
 w > current_state
 cline=$(wc -l < current_state)
-if [ "$oline" -ne "$cline" ]; then
+if [ "$oline" -lt "$cline" ]; then
         echo another user logged on | write $1 $2
-
+        rm ./initial_state current_state #you can add doggo here to commit suicide
+        break
+elif [ "$oline" -gt "$cline" ]; then
+        oline=$cline
+        echo another user logged off | write $1 $2
+fi
+if [ "$cline" -eq 2 ]; then
+        #it seems im alone
+        rm ./initial_state current_state #you can add doggo here to commit suicide
 fi
 done
